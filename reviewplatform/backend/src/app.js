654f043bb6fae8a29b29150646acb8601c25308c
mega123-art <agrawalparth326@gmail.com>
2025-06-26 22:23:35 +0530

    modified:   reviewplatform/backend/src/app.js

--- reviewplatform/backend/src/app.js
+++ reviewplatform/backend/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import leaderboardRoutes from "./routes/leaderboardRoutes.js";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 export default app;
+
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/leaderboard", leaderboardRoutes);
